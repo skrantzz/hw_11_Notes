61300d1de6d00239f2eb9d41283e95ffc96fa4c5
Sydney Krantz <syd107@gmail.com>
2020-03-20 17:16:48 -0700

    final commit

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,14 +1,35 @@
 var fs = require("fs");
-var journal = require("../db/journal");
+var path = require("path");
+var tableData = require("../db/db.json");
+// var journal = require("../db/journal");
 // var util = require("util");
 // const readFileAsync = util.promisify(fs.readFile);
 
 module.exports = function(app) {
   app.get("/api/notes", function(req, res) {
-    journal
-      .getNotes()
-      //   get notes is the async part
-      .then(notes => res.json(notes))
-      .catch(err => res.status(500).json(err));
+    res.json(tableData);
+  });
+
+  app.post("/api/notes", function(req, res) {
+    // .post 'creates' it
+    req.body.id = tableData.length + 1;
+    tableData.push(req.body);
+    // spread operator - converts the object to raw params - noah's way
+    // tableData.push({ id, ...req.body });
+    // console.log("hi 1", Object.assign({}, { id }, req.body));
+    // console.log("hi 2", { id, ...req.body });
+    res.json(true);
+  });
+
+  app.delete("/api/notes/:id", function(req, res) {
+    let deleteNote = tableData.filter(function(e) {
+      console.log(e.id == parseInt(req.params.id));
+      console.log(e.id, req.params.id);
+      return e.id !== parseInt(req.params.id);
+    });
+
+    tableData = deleteNote;
+
+    res.json(true);
   });
 };
